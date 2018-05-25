f6270b9991fcc026d209aa366a41764a13624b7d
Matthew Sampson <mbsampson89@gmail.com>
2018-05-25 14:46:12 -0700

    Set root page to reviews#index and configured Faker to provide seed data for Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+	Review.create!(
+		title: Faker::Coffee.blend_name,
+		movie_release_date: Faker::Date.between(365.days.ago, Date.today),
+		review_date: Faker::Date.between(7.days.ago, Date.today),
+		genre: "test",
+		user_email: Faker::Internet.email,
+		rating:  Faker::Number.between(1, 4)
+	)
+end 
