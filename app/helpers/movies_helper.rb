4f7dcd417dad4c1847f4e5a5fe9af14ccb9c86a0
Matthew Sampson <mbsampson89@gmail.com>
2018-06-06 13:29:26 -0700

    DRYed up code in reviews and movies views. Created partials and helpers to better structure code.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,10 @@
 module MoviesHelper
+
+	def generate_movie_tile(movie) 
+		link_to show_movie_path(:movie_id => movie.id) do 
+			("<img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2#{movie.poster_path}' class='movie-poster-image'>").html_safe +
+			("<p class='movie-list-title'>#{movie.title}</p>").html_safe
+		end	 	
+	end
+
 end
